c573695b5cdf035cd5647d82715d9f7babfa9e59
roywright5 <roy.wright05@ymail.com>
2024-09-07 16:18:28 -0400

    Preliminary check for Volley request, that works.

--- app/src/main/java/com/wrightr/trivia_2024/MainActivity.java
+++ app/src/main/java/com/wrightr/trivia_2024/MainActivity.java
@@ -1,6 +1,9 @@
 package com.wrightr.trivia_2024;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvTest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        tvTest = findViewById(R.id.tv_test);
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        String url = "https://www.google.com";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+
+                 response -> {
+
+                    tvTest.setText("200 response: " + response.substring(0, 300));
+                     Log.d("Response", "onCreate: " + response.substring(0, 500));
+
+                }, error -> {
+
+                    tvTest.setText("404 ERROR!!!");
+                }
+        );
+
+        queue.add(stringRequest);
+
+
+
+
+
     }
 }
