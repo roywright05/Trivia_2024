3ca5d914056e5dc7b8b766293b829db024154380
roywright5 <roy.wright05@ymail.com>
2024-09-07 17:32:55 -0400

    Set up MVC and added Singleton to appcontroller.java.

---
+++ app/src/main/java/com/wrightr/trivia_2024/controller/AppController.java
@@ -0,0 +1,67 @@
+package com.wrightr.trivia_2024.controller;
+
+import android.app.Application;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+public class AppController extends Application {
+    //when extending the Application class, go to manifest file, under application tag,
+    // name = class Name (e.g. AppController)
+
+    private static AppController instance;
+    private RequestQueue requestQueue;
+   // private ImageLoader imageLoader;
+    private static Context ctx;
+
+    private AppController(Context context) {
+        ctx = context;
+        requestQueue = getRequestQueue();
+
+//        imageLoader = new ImageLoader(requestQueue,
+//                new ImageLoader.ImageCache() {
+//                    private final LruCache<String, Bitmap>
+//                            cache = new LruCache<String, Bitmap>(20);
+//
+//                    @Override
+//                    public Bitmap getBitmap(String url) {
+//                        return cache.get(url);
+//                    }
+//
+//                    @Override
+//                    public void putBitmap(String url, Bitmap bitmap) {
+//                        cache.put(url, bitmap);
+//                    }
+//                });
+    }
+
+    public static synchronized AppController getInstance(Context context) {
+        if (instance == null) {
+            instance = new AppController(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+//    public ImageLoader getImageLoader() {
+//        return imageLoader;
+//    }
+
+}
