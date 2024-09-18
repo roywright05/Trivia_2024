e60ff321f0eef50999d247d1af9be692174eda2f
roywright5 <roy.wright05@ymail.com>
2024-09-18 11:34:56 -0400

    Eureka!  Button functionality is working fine now

--- app/src/main/java/com/wrightr/trivia_2024/MainActivity.java
+++ app/src/main/java/com/wrightr/trivia_2024/MainActivity.java
@@ -21,6 +21,7 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
 import com.wrightr.trivia_2024.controller.AppController;
 import com.wrightr.trivia_2024.data.AnswerListAsyncResponse;
 import com.wrightr.trivia_2024.data.Repository;
@@ -54,10 +55,16 @@ public class MainActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
 
 
-          questionList = new Repository().getQuestion(questionArrayList ->
+          questionList = new Repository().getQuestion(questionArrayList ->{
 
-                 binding.questionTv.setText(questionArrayList.get(currentQuestionIndex).getAnswer()));
-                //Log.d("Happy", "onCreate: " + questionArrayList));
+                      binding.questionTv.setText(questionArrayList.get(currentQuestionIndex)
+                              .getAnswer());
+
+                      updateCounter(questionArrayList);
+                  }
+          );
+
+
 
 
         binding.btnNext.setOnClickListener(view -> {
@@ -68,11 +75,11 @@ public class MainActivity extends AppCompatActivity {
         });
 
         binding.btnFalse.setOnClickListener(view -> {
-
+            checkAnswer(false);
         });
 
         binding.btnTrue.setOnClickListener(view -> {
-
+            checkAnswer(true);
         });
 
        // Log.d("Click", "onCreate: " + currentQuestionIndex);
@@ -80,9 +87,29 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void checkAnswer(boolean userChoseCorrect) {
+
+        boolean answer = questionList.get(currentQuestionIndex).isAnswerTrue();
+        int snackMessageID = 0;
+
+        if (userChoseCorrect == answer){
+            snackMessageID = R.string.correct_answer;
+        }else{
+            snackMessageID = R.string.incorrect_answer;
+        }
+
+        Snackbar.make(binding.cardView, snackMessageID, Snackbar.LENGTH_LONG).show();
+    }
+
+    private void updateCounter(ArrayList<Question> questionArrayList) {
+        binding.tvQuestionNum.setText("Question: " + currentQuestionIndex + "/"
+        + questionArrayList.size());
+    }
+
     private void updateQuestion() {
         String question = questionList.get(currentQuestionIndex).getAnswer();
         binding.questionTv.setText(question);
+        updateCounter((ArrayList<Question>) questionList);
     }
 
 
